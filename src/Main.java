a0cefa84c10a2ee7793f077f30ba33b4ed2473ce
Teddysir <sdzx0719@naver.com>
2025-01-12 23:55:48 +0900

    Chill guy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            String addr = input[0];
+            String password = input[1];
+
+            hashMap.put(addr, password);
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            String key = br.readLine();
+            String value = hashMap.get(key);
+            if(value != null) {
+                sb.append(value).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+
     }
+
+
 }
+
