b7e5b70003a326b73c66e79eb8b66a8645aba47f
Teddysir <sdzx0719@naver.com>
2025-01-15 17:04:55 +0900

    merge branch

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int N, M;
+    public static int count = 0;
+    public static int[][] node;
+    public static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
 
-        HashMap<String, String> hashMap = new HashMap<>();
-
-        for (int i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
-            String addr = input[0];
-            String password = input[1];
-
-            hashMap.put(addr, password);
-
-        }
-
-        StringBuilder sb = new StringBuilder();
+        node = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
 
         for (int i = 0; i < M; i++) {
-            String key = br.readLine();
-            String value = hashMap.get(key);
-            if(value != null) {
-                sb.append(value).append("\n");
-            }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int K = Integer.parseInt(st.nextToken());
+            int J = Integer.parseInt(st.nextToken());
+            node[K][J] = node[J][K] = 1;
         }
 
-        System.out.println(sb);
-
+        dfs(1);
+        System.out.println(count - 1);
     }
 
+    static void dfs(int K) {
+        visited[K] = true;
+        count += 1;
+
+        for (int i = 1; i <= N; i++) {
+            if (node[K][i] == 1 && !visited[i]) {
+                dfs(i);
+            }
+        }
+    }
 
 }
 
