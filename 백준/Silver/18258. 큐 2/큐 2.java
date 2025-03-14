c0ca17f1112be0081b329c480fd8c7298f298f74
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-03-14 19:10:07 +0900

    [Silver IV] Title: 큐 2, Time: 1256 ms, Memory: 350996 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String input = st.nextToken();
+
+            if (input.equals("push")) {
+                int num = Integer.parseInt(st.nextToken());
+                queue.add(num);
+            } else if (input.equals("pop")) {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else if (input.equals("size")) {
+                sb.append(queue.size()).append("\n");
+            } else if (input.equals("empty")) {
+                if (queue.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if (input.equals("front")) {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(queue.getFirst()).append("\n");
+                }
+            } else if (input.equals("back")) {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(queue.getLast()).append("\n");
+                }
+            }
+
+        }
+
+        System.out.println(sb);
+
+
+    }
+
+}
