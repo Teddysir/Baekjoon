17e05c9e4c0d70db311a13f4780ecba8faef303f
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-07-22 10:20:04 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 524 ms, Memory: 294336 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N, K;
+	static Deque<Integer> q = new LinkedList<>();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		for (int i = 1; i <= N; i++) {
+			q.add(i);
+		}
+
+		sb.append("<");
+		for (int i = 0; i < N - 1; i++) {
+			sb.append(solution()).append(", ");
+		}
+		sb.append(q.poll()).append(">");
+
+		System.out.println(sb);
+
+	}
+
+	static int solution() {
+		for (int i = 0; i < K-1; i++) {
+			q.add(q.pollFirst());
+		}
+
+		return q.pollFirst();
+	}
+
+}
