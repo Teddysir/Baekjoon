4125102f95843ac5541043ef6af7865520ec98c7
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-07-24 14:25:52 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,28 +1,35 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+	static int N, K, coin;
+	static int[] arr;
 
-        int N = in.nextInt();
-        int K = in.nextInt(); // K수 설정
+	public static void main(String args[]) throws Exception {
 
-        int coin[] = new int[N];
-        for (int i = 0; i < N; i++) {
-            coin[i] = in.nextInt(); // N개의 동전 갯수 정하기
-        }
-        int count = 0; // 경우의수 0으로 초기화
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // If문 안쪽 해석 한번 더하고 풀어보기
-        for (int i = N-1; i >= 0; i--) {
-            // 현재 동전의 가치가 K보다 작거나 같아야지 구성가능하다.
-            if(coin[i]<=K){
-                //현재 가치의 동전으로 구성할 수 있는 개수를 더해준다.
-                count = count + (K/coin[i]);
-                K = K % coin[i];
-            }
-        }
-        System.out.println(count);
-    }
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+
+		for (int i = N - 1; i >= 0; i--) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		coin = 0;
+		for (int i = 0; i < N; i++) {
+			if (arr[i] <= K) {
+				coin += (K / arr[i]);
+				int temp = (K / arr[i]);
+				K -= (temp * arr[i]);
+			}
+		}
+		System.out.println(coin);
+
+	}
 
 }
