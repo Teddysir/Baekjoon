1f39b6bdbcc25b7cd55fc6710b57c4879c35ea6e
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-07-25 11:36:47 +0900

    [Silver III] Title: 1로 만들기, Time: 188 ms, Memory: 56984 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,41 +3,56 @@ import java.util.*;
 
 public class Main {
 
-    public static Integer[] arr;
+	static int N, count;
+	static int[] arr = new int[1000001];
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String args[]) throws Exception {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 
-        arr = new Integer[N + 1];
-        arr[0] = arr[1] = 0;
+		count = 0;
 
-        System.out.println(solution(N));
+		Arrays.fill(arr, -1);
+		arr[0] = arr[1] = 0;
+		dp(N);
 
-    }
+		System.out.println(arr[N]);
 
-    static int solution(int N) {
+	}
 
-        if (arr[N] == null) {
-            if (N % 6 == 0) {
-                arr[N] = Math.min(solution(N - 1), Math.min(solution(N / 3), solution(N / 2))) + 1;
-            } else if (N % 3 == 0) {
-                arr[N] = Math.min(solution(N / 3), solution(N - 1)) + 1;
-            } else if (N % 2 == 0) {
-                arr[N] = Math.min(solution(N / 2), solution(N - 1)) + 1;
-            } else {
-                arr[N] = solution(N - 1) + 1;
-            }
-        }
-        return arr[N];
-    }
-    //  N = 2 = arr[2] = 1;
-    // N = 3 = arr[3] = 1
-    // N = 4 = arr[4] = 2
-    // N = 5 = arr[5] = 3
-    // N = 6 = arr[6] = 2
-    // N = 7 = arr[7] = 3
+	static int dp(int num) {
+//		System.out.println(num);
+//
+		if (arr[num] != -1) {
+			return arr[num];
+		}
+
+		if (num == 1) {
+			return arr[num] = 0;
+		}
+
+		// -1, %3, %2
+//		int result = num;
+		if (num % 6 == 0) {
+			arr[num] = Math.min(dp(num / 3) + 1, dp(num / 2) + 1);
+		}
+
+		arr[num] = dp(num - 1) + 1;
+		if (num % 3 == 0) {
+//			System.out.println("1 --- ");
+			arr[num] = Math.min(arr[num], dp(num / 3) + 1);
+
+		}
+		if (num % 2 == 0) {
+//			System.out.println("2 --- ");
+			arr[num] = Math.min(arr[num], dp(num / 2) + 1);
+
+		}
+
+		return arr[num];
+	}
 
 }
