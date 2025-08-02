09b0d1bb3fd0fa7376412e25362ebbbbde46ea0c
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-08-02 13:11:10 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 103 ms, Memory: 29,056 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	static int N, M, T, ans;
+	static int[][] friends;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		T = Integer.parseInt(br.readLine());
+
+		for (int k = 1; k <= T; k++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			ans = 0;
+
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+
+			friends = new int[N + 1][N + 1];
+			visited = new boolean[N + 1];
+
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+
+				friends[a][b] = friends[b][a] = 1;
+
+			}
+
+			for (int i = 1; i <= N; i++) {
+				if (!visited[i]) {
+					ans += dfs(i);
+				}
+			}
+
+			sb.append("#").append(k).append(" ").append(ans).append("\n");
+
+		}
+
+		System.out.println(sb);
+
+	}
+
+	static int dfs(int start) {
+
+		visited[start] = true;
+		for (int i = 1; i <= N; i++) {
+			if (friends[start][i] == 1 && !visited[i]) {
+				visited[i] = true;
+				dfs(i);
+
+			}
+
+		}
+		return 1;
+	}
+
+}
