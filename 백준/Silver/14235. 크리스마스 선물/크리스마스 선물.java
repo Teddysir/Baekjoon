e7b989d7d63c457b4f1ce00905a37ffaf8bb90d2
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-08-05 15:04:02 +0900

    [Silver III] Title: 크리스마스 선물, Time: 392 ms, Memory: 35700 KB -BaekjoonHub

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
@@ -4,8 +4,7 @@ import java.util.*;
 public class Main {
 
 	static int N;
-	static List<Integer> list = new ArrayList<Integer>();
-
+	static PriorityQueue<Integer> pq = new PriorityQueue<>();
 	public static void main(String args[]) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,16 +17,14 @@ public class Main {
 
 			int input = Integer.parseInt(st.nextToken());
 			if (input == 0) {
-				if (list.isEmpty()) {
+				if (pq.isEmpty()) {
 					sb.append(-1).append("\n");
 				} else {
-					Collections.sort(list, Collections.reverseOrder());
-					sb.append(list.get(0)).append("\n");
-					list.remove(0);
+					sb.append(-pq.poll()).append("\n");
 				}
 			} else {
 				for (int j = 0; j < input; j++) {
-					list.add(Integer.parseInt(st.nextToken()));
+					pq.add(-Integer.parseInt(st.nextToken()));
 				}
 			}
 		}
