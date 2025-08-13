db6c9a9427f368edbc0a73e2cf144481dd7999d9
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-08-13 11:28:35 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 114 ms, Memory: 30,316 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -3,92 +3,88 @@ import java.util.*;
 
 public class Solution {
 
-	static int TC, N;
+	static int TC, size, ans;
 	static int[][] map;
 	static int[][] dist;
-	static int ans;
+
 	static int[] dx = { 1, -1, 0, 0 };
 	static int[] dy = { 0, 0, 1, -1 };
 
 	static class Node implements Comparable<Node> {
-		int x, y, cost;
+		int x, y, weight;
 
-		Node(int x, int y, int cost) {
+		Node(int x, int y, int weight) {
 			this.x = x;
 			this.y = y;
-			this.cost = cost;
+			this.weight = weight;
 		}
 
 		@Override
 		public int compareTo(Node o) {
 			// TODO Auto-generated method stub
-			return this.cost - o.cost;
+			return this.weight - o.weight;
 		}
 	}
 
-	public static void main(String args[]) throws Exception {
-
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
 		TC = Integer.parseInt(br.readLine());
 
 		for (int k = 1; k <= TC; k++) {
 
-			N = Integer.parseInt(br.readLine());
-			map = new int[N][N];
-			dist = new int[N][N];
+			size = Integer.parseInt(br.readLine());
+			map = new int[size][size];
+			dist = new int[size][size];
+//			Arrays.fill(dist[size], Integer.MAX_VALUE);
 
-			for (int i = 0; i < N; i++) {
+			for (int i = 0; i < size; i++) {
 				String input = br.readLine();
-				for (int j = 0; j < N; j++) {
+				for (int j = 0; j < size; j++) {
 					map[i][j] = input.charAt(j) - 48;
 					dist[i][j] = Integer.MAX_VALUE;
 				}
 			}
-			ans = solution();
+			ans = 0;
+			dijkstra(new Node(0, 0, 0));
 
-			sb.append("#").append(k).append(" ").append(ans).append("\n");
+			sb.append("#").append(k).append(" ").append(dist[size - 1][size - 1]).append("\n");
 		}
-
 		System.out.println(sb);
-
 	}
 
-	static int solution() {
-		PriorityQueue<Node> pq = new PriorityQueue<Node>();
+	static void dijkstra(Node node) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(node);
 
-		dist[0][0] = map[0][0]; // -> 0으로 초기
-		pq.add(new Node(0, 0, map[0][0]));
+		map[0][0] = dist[0][0] = 0;
 
 		while (!pq.isEmpty()) {
+
 			Node temp = pq.poll();
 
 			int curX = temp.x;
 			int curY = temp.y;
-			int curCost = temp.cost;
+			int curWeight = temp.weight;
 
-			if (curCost > dist[curX][curY]) { // 들어온 비용이 원래있던 비용보다 크면비교할 필요 없다 - 기저조건?
+			if (curWeight > dist[curX][curY]) {
 				continue;
 			}
 
 			for (int i = 0; i < 4; i++) {
-				int nx = curX + dx[i];
-				int ny = curY + dy[i];
+				int nx = temp.x + dx[i];
+				int ny = temp.y + dy[i];
+				if (nx >= 0 && ny >= 0 && nx < size && ny < size) {
 
-				if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
-
-					int newCost = dist[curX][curY] + map[nx][ny];
-					if (newCost < dist[nx][ny]) {
-						dist[nx][ny] = newCost;
-						pq.add(new Node(nx, ny, newCost));
+					int newWeight = dist[curX][curY] + map[nx][ny];
+					if (dist[nx][ny] > newWeight) {
+						dist[nx][ny] = newWeight;
+						pq.add(new Node(nx, ny, newWeight));
 					}
-
 				}
 			}
 		}
 
-		return dist[N - 1][N - 1];
 	}
 
 }
