2a17e8785fccf114c44f61d60415a3d3caf4d719
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-08-17 23:52:32 +0900

    [D2] Title: 나무 높이, Time: 96 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -3,7 +3,6 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static Queue<Integer> even_q, odd_q;
 	static int N, ans, max;
 	static int[] tree;
 
@@ -13,53 +12,43 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine());
-			tree = new int[N];
-			max = 0;
+			N = Integer.parseInt(br.readLine()); // 나무 수
+			tree = new int[N]; // 나무 배열을 선언해주고
+			max = 0; // 최대 나무 높이 초기화
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
-				tree[i] = Integer.parseInt(st.nextToken());
-				max = Math.max(max, tree[i]);
+				tree[i] = Integer.parseInt(st.nextToken()); // 모든 나무를 입력받아주는
+				max = Math.max(max, tree[i]); // 최대높이의 나무를 초기화해준다.
 			} // 입력 끝
 
-			ans = 0;
+			ans = 0; // 정답도 초기화해주고,
 			solution();
 			System.out.println("#" + tc + " " + ans);
 		}
 	}
 
 	static void solution() {
-		even_q = new ArrayDeque<>();
-		odd_q = new ArrayDeque<>();
 
-		for (int num = 0; num < N; num++) {
-			if (tree[num] == max)
-				continue;
+		int day_1 = 0;
+		int day_2 = 0;
 
-			int n = max - tree[num];
-			// 초기화
-			for (int i = 0; i < n / 2; i++) {
-				even_q.offer(2);
-			}
-			if (n % 2 != 0)
-				odd_q.offer(1);
+		for (int i = 0; i < N; i++) {
+			day_2 += (max - tree[i]) / 2; // 짝수날이 필요한 만큼 더하고 
+			day_1 += (max - tree[i]) % 2; // 홀수날이 필요한 만큼 더한다음 
 		}
 
-		// 계산
-		if (even_q.size() >= odd_q.size()) {
-			while (true) {
-				if (even_q.size() <= odd_q.size()) {
-					ans = even_q.size() + odd_q.size();
-					break;
-				}
-				even_q.poll();
-				odd_q.offer(1);
-				odd_q.offer(1);
-			}
-		} else if (even_q.size() < odd_q.size()) {
-			ans += even_q.size() * 2;
-			int gap = odd_q.size() - even_q.size();
-			ans += gap + (gap - 1);
+		while (day_2 > day_1 + 1) { // 홀수날이 최대 짝수날보다 +1까지 많을만큼 일단 정리해주고 
+			day_2--;
+			day_1 += 2;
 		}
+
+		if (day_1 > day_2) { // 홀수날이 많으면 둘다 더한다음에 마지막 날은 어짜피 홀수날이면 +1 해주면 되니까 -1해서 하루 빼주고 
+			ans += day_1 + day_2 + ((day_1 - day_2) - 1);
+		} else if (day_2 > day_1) { // 만약 짝수 날이 많으면 일단 다 더한다음에 짝수날 - 홀수날 해야함 왜냐면 하루 더 먹어야함 
+			ans += day_1 + day_2 + (day_2 - day_1);
+		} else { // 같으면 동급 
+			ans += day_1 + day_2;
+		}
+
 	}
 }
