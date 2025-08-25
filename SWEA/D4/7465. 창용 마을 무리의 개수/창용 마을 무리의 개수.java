37aaf03bb91c7768bcde951dc14f74d1fddac456
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-08-25 15:29:24 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 98 ms, Memory: 28,288 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -3,58 +3,61 @@ import java.util.*;
 
 public class Solution {
 
-	static int N, M, T, ans;
-	static int[][] friends;
-	static boolean[] visited;
+	static int TC, ans, N, M;
+	static int[] parent;
+	static Set<Integer> set;
 
 	public static void main(String[] args) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		T = Integer.parseInt(br.readLine());
+		TC = Integer.parseInt(br.readLine());
+		for (int k = 1; k <= TC; k++) {
 
-		for (int k = 1; k <= T; k++) {
+			// 0 은 합집합
+			// 1은 포함되어있는지?
+
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			ans = 0;
-
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 
-			friends = new int[N + 1][N + 1];
-			visited = new boolean[N + 1];
+			parent = new int[N + 1];
+			for (int i = 1; i <= N; i++) {
+				parent[i] = i;
+			}
 
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
+				int tempX = Integer.parseInt(st.nextToken());
+				int tempY = Integer.parseInt(st.nextToken());
 
-				friends[a][b] = friends[b][a] = 1;
+				union(tempX, tempY);
+
+			}
+			set = new HashSet<>();
+			for(int i = 1; i <= N; i++) {
+				set.add(find(i));
 			}
 
-			for (int i = 1; i <= N; i++) {
-				if (!visited[i]) {
-					dfs(i);
-					ans++;
-				}
-			}
-
-			sb.append("#").append(k).append(" ").append(ans).append("\n");
-
+			sb.append("#").append(k).append(" ").append(set.size()).append("\n");
 		}
-
 		System.out.println(sb);
-
 	}
 
-	static void dfs(int start) {
-		visited[start] = true;
-		for (int i = 1; i <= N; i++) {
-			if (friends[start][i] == 1 && !visited[i]) {
-				visited[i] = true;
-				dfs(i);
-			}
+	static void union(int x, int y) {
+		int nx = find(x);
+		int ny = find(y);
+		if (nx != ny) {
+			parent[ny] = nx;
 		}
 	}
 
+	static int find(int x) {
+		if (parent[x] == x) {
+			return x;
+		} else {
+			return parent[x] = find(parent[x]);
+		}
+	}
 }
