b8a28ba6d13faad9f2a1ab1a2d4b0db331375645
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-08-31 21:51:00 +0900

    [Silver II] Title: 나무 자르기, Time: 732 ms, Memory: 175276 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -3,48 +3,60 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+	static int N;
+	static long ans, get;
+	static int[] trees;
+	static int max;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	public static void main(String[] args) throws Exception {
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-        int[] trees = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine(), " ");
-        int max = 0;
-        int min = 0;
+		N = Integer.parseInt(st.nextToken());
+		get = Integer.parseInt(st.nextToken());
+		trees = new int[N];
 
-        for (int i = 0; i < N; i++) {
-            trees[i] = Integer.parseInt(st.nextToken());
-            if(trees[i] > max) {
-                max = trees[i];
-            }
-        }
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			trees[i] = Integer.parseInt(st.nextToken());
+			if (trees[i] > max) {
+				max = trees[i]; //일단 최대높이 정해주고 
+			}
+		}
+		Arrays.sort(trees); // 정렬ㄹ도 꼭 해야하고 
 
-        while (min < max) {
-            int mid = (min + max) / 2;
-            long total = 0;
+		solution();
 
-            for (int tree : trees) {
-                if (tree - mid > 0) {
-                    total += (tree - mid);
-                }
-            }
+		System.out.println(ans);
 
-            if (total < M) {
-                max = mid;
-            } else {
-                min = mid + 1;
-            }
+	}
 
-        }
-        System.out.println(min - 1);
+	static void solution() {
 
+		int left = 0; // 최소 
+		int right = max; // 최대 
 
-    }
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			long total = 0;
 
+			for (int tree : trees) { // 나무들중에서 
+				if (tree > mid) { // 나무가 기준보다 크면 자를 수 있으니까 
+					total += tree - mid;
+				}
+			}
+
+			if (total < get) { // 임시로 자른 나무가, 내가 얻어야하는 나무보다 적다면 
+				right = mid - 1; // 톱날의 길이를 줄인다. -> 그래야지 더 잘리니까. 
+			} else if (total >= get) {
+				ans = mid; // 
+				left = mid + 1; // 넘치면 톱날의 길이를 높이면 되는데 이때 
+			} 
+		}
+
+	}
 
 }
