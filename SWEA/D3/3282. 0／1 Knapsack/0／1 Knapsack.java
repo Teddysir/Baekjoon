0f843688ea2bd82782636ef14c2da0edb0047455
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-09-08 11:26:42 +0900

    [D3] Title: 0/1 Knapsack, Time: 109 ms, Memory: 28,968 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,66 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
 
-	static int T, max_volume, max_value, total_bag, ans;
-	static HashMap<Integer, int[]> map;
-	static int[][] arr;
+	static int TC, ans, N, K;
+	static int[] volumes, values;
+	static int[] dp;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		T = Integer.parseInt(br.readLine());
-
-		for (int test_case = 1; test_case <= T; test_case++) {
+		TC = Integer.parseInt(br.readLine());
+		for (int k = 1; k <= TC; k++) {
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			total_bag = Integer.parseInt(st.nextToken());
-			max_volume = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
 
-			ans = 0;
-			map = new HashMap<Integer, int[]>();
-			arr = new int[total_bag + 1][max_volume + 1];
-//			for (int i = 0; i <= total_bag; i++) {
-//				Arrays.fill(arr[i], -1);
-//			}
+			volumes = new int[N + 1];
+			values = new int[N + 1];
+			dp = new int[K + 1];
 
-			for (int i = 0; i < total_bag; i++) {
+			for (int i = 1; i <= N; i++) {
 				st = new StringTokenizer(br.readLine());
-				int volume = Integer.parseInt(st.nextToken());
-				int value = Integer.parseInt(st.nextToken());
-				map.put(i, new int[] { volume, value });
+				volumes[i] = Integer.parseInt(st.nextToken());
+				values[i] = Integer.parseInt(st.nextToken());
 			}
-			ans = dp(0, 0);
 
-			sb.append("#").append(test_case).append(" ").append(ans).append("\n");
+			for (int i = 1; i <= N; i++) {
+				for (int j = K; j >= volumes[i]; j--) {
+					dp[j] = Math.max(dp[j], dp[j - volumes[i]] + values[i]);
+				}
+			}
+
+			sb.append("#").append(k).append(" ").append(dp[K]).append("\n");
 
 		}
-
 		System.out.println(sb);
-
-	}
-
-	static int dp(int count, int volume) {
-
-		if(arr[count][volume] != 0) {
-			return arr[count][volume]; // 이미 있는거면 계산할 필요 없다.
-		}
-		
-		if(count == total_bag) {
-			return 0; // 마지막은 끗 
-		}
-		
-		int result = dp(count+1, volume); // -> 그냥 넘어갈때,
-		int[] temp = map.get(count);
-		if(volume + temp[0] <= max_volume) { // 안넘으면 
-			result = Math.max(result, dp(count+1, volume + temp[0]) + temp[1]);
-		}
-		
-		arr[count][volume] = result;
-		return result;
 	}
 
 }
