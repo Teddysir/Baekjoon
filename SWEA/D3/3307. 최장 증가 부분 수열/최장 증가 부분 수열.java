9c2b841800d1124edf4e5e6b27d095e6370cf395
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-09-09 11:32:40 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 126 ms, Memory: 33,512 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,49 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Solution {
+public class Solution {
 
-	static int T, N, max;
-	static int[] arr, dp;
+	static int TC, N;
+	static int[] arr;
+	static int[] answer;
 
-	public static void main(String args[]) throws Exception {
+	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		T = Integer.parseInt(br.readLine());
-		for (int i = 1; i <= T; i++) {
+		TC = Integer.parseInt(br.readLine());
+		for (int k = 1; k <= TC; k++) {
 
 			N = Integer.parseInt(br.readLine());
-            max = 0;
-
 			arr = new int[N];
-			dp = new int[N];
-			for (int j = 0; j < N; j++) {
-				dp[j] = 1;
-			}
+			answer = new int[N];
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				arr[j] = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
-			for (int j = 1; j < N; j++) {
-				for (int k = 0; k < j; k++) {
-					if (arr[j] > arr[k]) {
-						dp[j] = Math.max(dp[j], dp[k] + 1);
-					}
+			int size = 0;
+
+			for (int i = 0; i < N; i++) {
+				int idx = Arrays.binarySearch(answer, 0, size, arr[i]);
+				if (idx < 0) {
+					idx = (idx * -1) - 1;
+				}
+				answer[idx] = arr[i];
+				if (idx == size) {
+					size++;
 				}
 			}
 
-			for (int j = 0; j < dp.length; j++) {
-				max = Math.max(max, dp[j]);
-			}
+			sb.append("#").append(k).append(" ").append(size).append("\n");
 
-			sb.append("#").append(i).append(" ").append(max).append("\n");
 		}
-
 		System.out.println(sb);
-
 	}
 
 }
