78fc031fb24fac51b03708cbf979f4cb5eddcb04
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-09-12 23:02:12 +0900

    [D3] Title: 0/1 Knapsack, Time: 90 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -2,10 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+	static int TC, ans, limit, count;
 
-	static int TC, ans, N, K;
-	static int[] volumes, values;
-	static int[] dp;
+	static int[] dp_1;
+	static int[][] dp_2;
+
+	static int[] volumes;
+	static int[] values;
 
 	public static void main(String[] args) throws Exception {
 
@@ -13,33 +16,47 @@ public class Solution {
 		StringBuilder sb = new StringBuilder();
 
 		TC = Integer.parseInt(br.readLine());
-		for (int k = 1; k <= TC; k++) {
+
+		for (int t = 1; t <= TC; t++) {
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			N = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
+			count = Integer.parseInt(st.nextToken());
+			limit = Integer.parseInt(st.nextToken());
 
-			volumes = new int[N + 1];
-			values = new int[N + 1];
-			dp = new int[K + 1];
+			volumes = new int[count];
+			values = new int[count];
 
-			for (int i = 1; i <= N; i++) {
+			dp_1 = new int[limit + 1]; // 1차원 디피 최대 무게
+			dp_2 = new int[count][limit + 1]; // 2차원디피 최대무게
+
+			for (int i = 0; i < count; i++) {
 				st = new StringTokenizer(br.readLine());
 				volumes[i] = Integer.parseInt(st.nextToken());
 				values[i] = Integer.parseInt(st.nextToken());
 			}
 
-			for (int i = 1; i <= N; i++) {
-				for (int j = K; j >= volumes[i]; j--) {
-					dp[j] = Math.max(dp[j], dp[j - volumes[i]] + values[i]);
-				}
-			}
+			dp_1_solution();
+//			dp_2_solution();
 
-			sb.append("#").append(k).append(" ").append(dp[K]).append("\n");
-
+			sb.append("#").append(t).append(" ").append(dp_1[limit]).append("\n");
 		}
+
 		System.out.println(sb);
+	}
+
+	static void dp_1_solution() {
+
+		for (int i = 0; i < count; i++) {
+			for (int j = limit; j >= volumes[i]; j--) {
+				dp_1[j] = Math.max(dp_1[j], dp_1[j - volumes[i]] + values[i]);
+			}
+		}
+
+	}
+
+	static void dp_2_solution() {
+
 	}
 
 }
