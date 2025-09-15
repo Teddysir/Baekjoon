e69bdf13b3ef08b6e9a748fee6fd3c50ce07306b
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-09-15 11:06:55 +0900

    [Gold V] Title: 최소비용 구하기, Time: 1804 ms, Memory: 48244 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int N, M;
+	static int[][] map;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+
+		map = new int[N + 1][N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (i == j) {
+					map[i][j] = 0;
+				} else {
+					map[i][j] = 100_100_1001;
+				}
+			}
+		}
+
+		for (int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			map[a][b] = Math.min(map[a][b], cost);
+		}
+
+		for (int k = 1; k <= N; k++) {
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+				}
+			}
+		}
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
+
+		System.out.println(map[start][end]);
+
+	}
+
+}
