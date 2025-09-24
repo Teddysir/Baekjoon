7fa87fcd0d000f6d00846f6267c893ec5fbb732a
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-09-24 21:31:40 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 384 ms, Memory: 40588 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -2,77 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static int[][] forest;
+    static int[][] dp;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-	static int N, ans;
-	static int[][] map;
-	static int[][] dp;
-	static int[] dr = { 1, -1, 0, 0 };
-	static int[] dc = { 0, 0, 1, -1 };
-	static PriorityQueue<Pos> pq = new PriorityQueue<Pos>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        forest = new int[N][N];
+        dp = new int[N][N];
 
-	static class Pos implements Comparable<Pos> {
-		int r, c, value;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                forest[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		Pos(int r, int c, int value) {
-			this.r = r;
-			this.c = c;
-			this.value = value;
-		}
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                answer = Math.max(answer, dfs(i, j));
+            }
+        }
 
-		@Override
-		public int compareTo(Pos o) {
-			return this.value - o.value;
-		}
+        System.out.println(answer);
+    }
 
-	}
+    static int dfs(int r, int c) {
+        // 이미 방문했으면 저장된 값 반환
+        if (dp[r][c] != 0) return dp[r][c];
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        dp[r][c] = 1; // 최소 1일(현재 위치)
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
 
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		dp = new int[N][N];
-		ans = -1;
+            if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+            if (forest[nr][nc] > forest[r][c]) {
+                dp[r][c] = Math.max(dp[r][c], dfs(nr, nc) + 1);
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				map[i][j] = num;
-				pq.add(new Pos(i, j, num));
-				dp[i][j] = 1;
-			}
-		}
-
-		while (!pq.isEmpty()) {
-			Pos p = pq.poll();
-			solution(p.r, p.c);
-		}
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				ans = Math.max(ans, dp[i][j]);
-			}
-		}
-
-		System.out.println(ans);
-
-	}
-
-	static void solution(int r, int c) {
-
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-
-			if (nr >= 0 && nc >= 0 && nr < N && nc < N) {
-				if (map[nr][nc] < map[r][c]) {
-					dp[r][c] = Math.max(dp[r][c], dp[nr][nc] + 1);
-				}
-			}
-
-		}
-
-	}
-
+        return dp[r][c];
+    }
 }
