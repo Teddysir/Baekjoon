61fa9d4fa6c52a9c7332bcdc8c5aa498737af719
김성훈 <97444407+Teddysir@users.noreply.github.com>
2025-10-23 11:09:37 +0900

    [Gold I] Title: 청소년 상어, Time: 64 ms, Memory: 11636 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.java
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.java
@@ -3,163 +3,198 @@ import java.util.*;
 
 public class Main {
 
-    static int[] dr = { 0, -1, -1, 0, 1, 1, 1, 0, -1 }; // 무방향, 위, 왼대, 왼, 왼아, 아, 아오, 오, 오위
-    static int[] dc = { 0, 0, -1, -1, -1, 0, 1, 1, 1 }; // 0 무방향 1 ~ 8 ..
-    static int ans = 0;
+	static int[] dr = { 0, -1, -1, 0, 1, 1, 1, 0, -1 }; // 무방향, 위, 왼대, 왼, 왼아, 아, 아오, 오, 오위
+	static int[] dc = { 0, 0, -1, -1, -1, 0, 1, 1, 1 }; // 0 무방향 1 ~ 8 ..
+	static int ans = 0;
 
-    static class Fish {
-        int num, vector;
+	static class Fish {
+		int num, vector;
 
-        Fish(int num, int vector) {
-            this.num = num;
-            this.vector = vector;
-        }
+		Fish(int num, int vector) {
+			this.num = num;
+			this.vector = vector;
+		}
 
-        void setter(int input_num, int input_vector) {
-            this.num = input_num;
-            this.vector = input_vector;
-        }
+		void setter(int input_num, int input_vector) {
+			this.num = input_num;
+			this.vector = input_vector;
+		}
 
-        void set_vector(int input_vector) {
-            this.vector = input_vector;
-        }
-    }
+		void set_vector(int input_vector) {
+			this.vector = input_vector;
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-        Fish[][] origin_map = new Fish[4][4];
+		Fish[][] origin_map = new Fish[4][4];
+		// 4x4 의 맵과 1~8까지의 백터를 저장하는 3차원 배열,
 
-        for (int i = 0; i < 4; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 4; j++) {
-                int fish_num = Integer.parseInt(st.nextToken());
-                int vector = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < 4; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 4; j++) {
+				int fish_num = Integer.parseInt(st.nextToken());
+				int vector = Integer.parseInt(st.nextToken());
 
-                origin_map[i][j] = new Fish(fish_num, vector);
-            }
-        }
+				origin_map[i][j] = new Fish(fish_num, vector);
+			}
+		}
 
-        // 상어가 (0,0) 물고기를 먹고 시작
-        int first_eat = origin_map[0][0].num;
-        int shark_dir = origin_map[0][0].vector;
-        origin_map[0][0] = new Fish(20, shark_dir); // 상어 표시
+		int temp_ans = origin_map[0][0].num; // 처음 0,0 값 먹고.
+		boolean[] death = new boolean[17]; // 먹은거지워주고
+		death[origin_map[0][0].num] = true; // death 배열에서 체크
+		origin_map[0][0] = new Fish(20, origin_map[0][0].vector); // 상어 만들어주고, 상어는 20번
 
-        boolean[] death = new boolean[17];
-        death[first_eat] = true;
+		start(origin_map, death, temp_ans);
 
-        start(origin_map, death, 0, 0, first_eat);
+		System.out.println(ans);
 
-        System.out.println(ans);
-    }
+	}
 
-    static void start(Fish[][] input_map, boolean[] death, int shark_r, int shark_c, int sum) {
-        // 현재 상태 저장 (깊은 복사)
-        Fish[][] copied_map = copy_map(input_map);
-        boolean[] copied_death = death.clone();
+	static void start(Fish[][] map, boolean[] death, int temp_ans) {
 
-        // 물고기 이동
-        fish_swap(copied_map, copied_death);
+		Fish[][] copy_map = arr_copy(map); // 깊은 복사로 배열을 만들어준다,
+		Fish shark = null;
+		int method_ans = temp_ans; // 첫번째 때문에 값을 복사해준다,
+		int r = -1; // 상어 좌표 행
+		int c = -1; // 상어 좌표 열
+		fish_swap(copy_map, death); // 먹고 들어왔으니까 일단 물고기 돌려주고,
 
-        // 상어가 이동할 수 있는 모든 경우 탐색
-        int shark_vector = copied_map[shark_r][shark_c].vector;
-        boolean can_move = false;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (copy_map[i][j] != null) {
+					if (copy_map[i][j].num == 20) { // 상어를 찾아준다 배열에서,
+						shark = new Fish(copy_map[i][j].num, copy_map[i][j].vector);
+						r = i;
+						c = j;
+					}
+				}
+			}
+		}
+//		for (int i = 0; i < 4; i++) {
+//			for (int j = 0; j < 4; j++) {
+//				if (copy_map[i][j] != null) {
+//					System.out.print(copy_map[i][j].num + " ");
+//				}
+//			}
+//			System.out.println();
+//		}
+//		System.out.println(" ------ ");
 
-        for (int i = 1; i <= 3; i++) {
-            int nr = shark_r + dr[shark_vector] * i;
-            int nc = shark_c + dc[shark_vector] * i;
+		for (int i = 1; i <= 3; i++) {
+			int nr = r + (dr[shark.vector] * i);
+			int nc = c + (dc[shark.vector] * i);
 
-            if (nr < 0 || nc < 0 || nr >= 4 || nc >= 4) break;
+			if (nr < 0 || nc < 0 || nr >= 4 || nc >= 4) {
+				continue;
+			}
+			if (copy_map[nr][nc] == null) {
+				continue;
+			}
 
-            if (copied_map[nr][nc] != null && copied_map[nr][nc].num != 20) {
-                can_move = true;
+			if (copy_map[nr][nc] != null) {
 
-                // 상어가 해당 물고기를 먹음
-                int eat_fish = copied_map[nr][nc].num;
-                int new_shark_dir = copied_map[nr][nc].vector;
+				Fish remember = new Fish(copy_map[nr][nc].num, copy_map[nr][nc].vector);
+				copy_map[nr][nc] = new Fish(20, remember.vector); // 이거 깊은복사 되는지 체크,
+				copy_map[r][c] = null;
+				death[remember.num] = true;
+//				System.out.println(temp_ans + remember.num + "값 ");
+//				System.out.println();
+				start(copy_map, death, temp_ans + remember.num);
 
-                Fish[][] next_map = copy_map(copied_map);
-                boolean[] next_death = copied_death.clone();
+				// 백트래킹,
+				copy_map[r][c] = shark;
+				copy_map[nr][nc] = remember;
+				death[remember.num] = false;
 
-                next_map[shark_r][shark_c] = null; // 상어 원래 위치 비우기
-                next_map[nr][nc] = new Fish(20, new_shark_dir); // 새 위치에 상어
-                next_death[eat_fish] = true;
+//				move_count++;
+			}
+		}
 
-                start(next_map, next_death, nr, nc, sum + eat_fish);
-            }
-        }
+		ans = Math.max(method_ans, ans);
 
-        // 더 이상 이동할 수 없으면 최댓값 갱신
-        if (!can_move) {
-            ans = Math.max(ans, sum);
-        }
-    }
+	}
 
-    static void fish_swap(Fish[][] input_map, boolean[] death) {
-        for (int i = 1; i <= 16; i++) {
-            if (!death[i]) {
-                // 물고기 i를 찾기
-                out: for (int j = 0; j < 4; j++) {
-                    for (int k = 0; k < 4; k++) {
-                        if (input_map[j][k] != null && input_map[j][k].num == i) {
-                            int vector = input_map[j][k].vector;
-                            int vector_find_count = 0;
+	static void fish_swap(Fish[][] input_map, boolean[] death) {
 
-                            while (vector_find_count < 8) {
-                                int nr = j + dr[vector];
-                                int nc = k + dc[vector];
+		for (int i = 1; i <= 16; i++) {
+			if (!death[i]) {
 
-                                // 범위 체크
-                                if (nr < 0 || nc < 0 || nr >= 4 || nc >= 4) {
-                                    vector_find_count++;
-                                    vector = (vector % 8) + 1;
-                                    continue;
-                                }
+				// TODO : 스왑,
 
-                                // 상어가 있는 곳은 이동 불가
-                                if (input_map[nr][nc] != null && input_map[nr][nc].num == 20) {
-                                    vector_find_count++;
-                                    vector = (vector % 8) + 1;
-                                    continue;
-                                }
+				out: for (int j = 0; j < 4; j++) {
+					for (int k = 0; k < 4; k++) {
+						if (input_map[j][k] != null) { // 빈칸이 아니고,
+							if (input_map[j][k].num == i) { // 해당 번호와 현재 맵의 fish 번호가 같다면,
 
-                                // 빈 칸으로 이동
-                                if (input_map[nr][nc] == null) {
-                                    input_map[nr][nc] = input_map[j][k];
-                                    input_map[nr][nc].set_vector(vector);
-                                    input_map[j][k] = null;
-                                    break out;
-                                }
+								int vector = input_map[j][k].vector; // 해당 물고기의 방향과
+								int vector_find_count = 0; // 방향을 돌리는 카운트
 
-                                // 다른 물고기와 스왑
-                                if (input_map[nr][nc] != null) {
-                                    Fish temp = input_map[nr][nc];
-                                    input_map[nr][nc] = input_map[j][k];
-                                    input_map[j][k] = temp;
-                                    input_map[nr][nc].set_vector(vector);
-                                    break out;
-                                }
-                            }
+								while (vector_find_count < 9) {
 
-                            // 8방향 모두 막혔으면 제자리
-                            break out;
-                        }
-                    }
-                }
-            }
-        }
-    }
+									if (vector == 0) {
+										vector++;
+									}
 
-    // 맵 깊은 복사
-    static Fish[][] copy_map(Fish[][] input_map) {
-        Fish[][] new_map = new Fish[4][4];
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (input_map[i][j] != null) {
-                    new_map[i][j] = new Fish(input_map[i][j].num, input_map[i][j].vector);
-                }
-            }
-        }
-        return new_map;
-    }
+									int nr = j + dr[vector]; // 만약 다음 방향이 가능한지계속 체크
+									int nc = k + dc[vector];
+
+									if (nr < 0 || nc < 0 || nr >= 4 || nc >= 4) {
+										vector_find_count++;
+										vector = (vector % 8) + 1;
+										continue; // 맵 밖이면 넘기고
+									}
+
+									if (input_map[nr][nc] == null) {
+										input_map[nr][nc] = input_map[j][k];
+										input_map[nr][nc].set_vector(vector);
+										input_map[j][k] = null;
+										break out;
+									}
+
+									if (input_map[nr][nc].num == 20) { // 상어면 넘기고
+										vector_find_count++;
+										vector = (vector % 8) + 1;
+										continue;
+									}
+
+									if (input_map[nr][nc] != null) {
+										Fish temp = input_map[nr][nc];
+										input_map[nr][nc] = input_map[j][k];
+										input_map[j][k] = temp;
+										input_map[nr][nc].set_vector(vector);
+										break out;
+									}
+
+								}
+
+							}
+						}
+
+					}
+				}
+
+			}
+		}
+
+	}
+
+	static Fish[][] arr_copy(Fish[][] map) {
+
+		Fish[][] copy_map = new Fish[4][4];
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (map[i][j] != null) {
+					Fish temp = new Fish(map[i][j].num, map[i][j].vector);
+					copy_map[i][j] = temp;
+				}
+			}
+		}
+
+		return copy_map;
+	}
+
 }
