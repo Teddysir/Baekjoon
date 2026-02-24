4d0dba732f15b1cbd3c19126ea4d726d7b4fb87d
김성훈 <97444407+Teddysir@users.noreply.github.com>
2026-02-24 16:04:25 +0900

    [Gold IV] Title: 최단경로, Time: 620 ms, Memory: 114848 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,44 +1,47 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
-	static int ans, V, E, start;
-	static int[] dist;
-	static List<Node>[] nodeList;
+	static class Node implements Comparable<Node> {
+		int next;
+		int weight;
 
-	static class Node implements Comparable<Node>{
-		int to, weight;
-
-		Node(int to, int weight) {
-			this.to = to;
+		Node(int next, int weight) {
+			this.next = next;
 			this.weight = weight;
 		}
 
 		@Override
 		public int compareTo(Node o) {
-			// TODO Auto-generated method stub
 			return Integer.compare(this.weight, o.weight);
 		}
 	}
 
+	static StringTokenizer st;
+	static int V, E, start;
+	static List<Node>[] graph;
+	static int[] dist;
+
 	public static void main(String[] args) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
+
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 
-		nodeList = new ArrayList[V + 1];
-		for (int i = 0; i <= V; i++) {
-			nodeList[i] = new ArrayList<Node>();
-		}
+		graph = new ArrayList[V + 1];
 		dist = new int[V + 1];
 
 		Arrays.fill(dist, Integer.MAX_VALUE);
 
+		for (int i = 0; i <= V; i++) {
+			graph[i] = new ArrayList<Node>();
+		}
+		
 		start = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < E; i++) {
@@ -47,11 +50,12 @@ public class Main {
 			int to = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
 
-			nodeList[from].add(new Node(to, weight));
-
+			graph[from].add(new Node(to, weight));
 		}
-		djikstra(start);
 
+
+		djikstra(start);
+		
 		for (int i = 1; i <= V; i++) {
 			if (dist[i] == Integer.MAX_VALUE) {
 				sb.append("INF").append("\n");
@@ -59,33 +63,32 @@ public class Main {
 				sb.append(dist[i]).append("\n");
 			}
 		}
+
 		System.out.println(sb);
+
 	}
 
 	static void djikstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
+		PriorityQueue<Node> pq = new PriorityQueue<Node>();
 		pq.add(new Node(start, 0));
 		dist[start] = 0;
 
 		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
 
-			Node temp = pq.poll();
-			int nextTo = temp.to;
-			int nextWeight = temp.weight;
-
-			if (dist[nextTo] < nextWeight) {
+			if (cur.weight > dist[cur.next])
 				continue;
-			}
 
-			for (Node node : nodeList[nextTo]) {
-				int next = node.weight + nextWeight;
-				if (dist[node.to] > next) {
-					dist[node.to] = next;
-					pq.add(new Node(node.to, next));
+			for (Node next : graph[cur.next]) {
+				int newWeight = cur.weight + next.weight;
+
+				if (newWeight < dist[next.next]) {
+					dist[next.next] = newWeight;
+					pq.add(new Node(next.next, newWeight));
 				}
 			}
-
 		}
 
 	}
+
 }
