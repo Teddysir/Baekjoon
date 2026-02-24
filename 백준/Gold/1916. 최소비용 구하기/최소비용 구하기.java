e9c74f191ab2e7ef11308b8851c92f5095416d9a
김성훈 <97444407+Teddysir@users.noreply.github.com>
2026-02-24 14:25:15 +0900

    [Gold V] Title: 최소비용 구하기, Time: 428 ms, Memory: 51660 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,53 +1,83 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
+	static class Node implements Comparable<Node> {
+		int next;
+		int weight;
+
+		Node(int next, int weight) {
+			this.next = next;
+			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+
+	}
+
+	static StringTokenizer st;
 	static int N, M;
-	static int[][] map;
+	static int[] dist;
+	static List<Node>[] graph;
 
 	public static void main(String[] args) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine()); // 정점의 수
+		M = Integer.parseInt(br.readLine()); // 간선의 수
 
-		map = new int[N + 1][N + 1];
+		dist = new int[N + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (i == j) {
-					map[i][j] = 0;
-				} else {
-					map[i][j] = 100_100_1001;
-				}
-			}
+		graph = new ArrayList[N + 1];
+
+		for (int i = 1; i <= N; i++) { // 이렇게 일단 다 만들어주고
+			graph[i] = new ArrayList<Node>();
 		}
 
-		for (int i = 0; i < M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			map[a][b] = Math.min(map[a][b], cost);
+		for (int i = 0; i < M; i++) { // 간선 입력 받아주기
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			graph[from].add(new Node(to, weight));
 		}
 
-		for (int k = 1; k <= N; k++) {
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-					map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
-				}
-			}
-		}
+		st = new StringTokenizer(br.readLine());
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		int start = Integer.parseInt(st.nextToken());
 		int end = Integer.parseInt(st.nextToken());
 
-		System.out.println(map[start][end]);
+		djikstar(start);
 
+		System.out.println(dist[end]);
+	}
+
+	static void djikstar(int start) {
+		PriorityQueue<Node> pq = new PriorityQueue<Main.Node>();
+		pq.add(new Node(start, 0));
+		dist[start] = 0;
+
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+
+			if (cur.weight > dist[cur.next])
+				continue;
+
+			for (Node nextNode : graph[cur.next]) {
+				int newWeight = cur.weight + nextNode.weight;
+
+				if (newWeight < dist[nextNode.next]) {
+					dist[nextNode.next] = newWeight;
+					pq.add(new Node(nextNode.next, newWeight));
+				}
+			}
+		}
 	}
 
 }
